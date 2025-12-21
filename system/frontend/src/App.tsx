7cc6841730f3bc19887cc8d4fad7520770f90e7e
hudsonmp <hudsonmitchellpullman@gmail.com>
2025-12-21 12:45:54 -0800

    created frontend and ran backend tests with simPY

---
+++ system/frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container container-1">
+        {/* Container 1 */}
+      </div>
+      <div className="container container-2">
+        {/* Container 2 */}
+      </div>
+      <div className="container container-3">
+        {/* Container 3 */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
